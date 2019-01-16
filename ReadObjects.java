2082d5059cab9d21d30aebc63b51820c0cad2632
aezBurkhal <44988851+aezBurkhal@users.noreply.github.com>
2019-01-16 14:10:02 -0500

    Add files via upload

---
+++ ReadObjects.java
@@ -0,0 +1,65 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+
+
+//use serializing to save and load functionality in program
+//how to de-serialize array lists 
+
+public class ReadObjects {
+	
+	public static void main(String[] args) {
+		System.out.println("reading objects....");
+		
+		try(FileInputStream fi = new FileInputStream("people.bin")) {
+			
+			
+			ObjectInputStream os = new ObjectInputStream(fi);
+			
+			Person[] people = (Person[])os.readObject();
+			
+			@SuppressWarnings("unchecked")
+			ArrayList<Person> peopleList = (ArrayList<Person>)os.readObject();
+			
+			
+			for(Person person: people) {
+				System.out.println(person);
+			}
+			for(Person person: peopleList) {
+				System.out.println(person);
+			}
+			
+			int num = os.readInt();
+			
+			for(int i=0; i<num; i++) {
+				Person person = (Person)os.readObject();
+				System.out.println(person);
+			}
+			
+			
+			os.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+	
+		
+	
+			
+				
+	
+
